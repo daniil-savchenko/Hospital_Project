86304a6681414e02605618358caf1c580963b8ad
lol lol <Denislavbg7@gmail.com>
2023-03-28 22:33:44 +0300

    sdubmit

--- Hospital_Project/ReservationForm.Designer.cs
+++ Hospital_Project/ReservationForm.Designer.cs
@@ -87,7 +87,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 29);
             this.button1.TabIndex = 11;
-            this.button1.Text = "button1";
+            this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,52 +1,12 @@
-﻿Create table Positions(
-	ID int not null primary key,
-	posName varchar(20)
-)
+﻿SELECT * FROM Workers
 
-create table Workers(
-	ID int not null primary key,
-	workerName varchar(20),
-	phone varchar(10),
-	email varchar(20),
-	Position int,
-	salary float,
+SELECT * FROM DOCTORS
 
-	Foreign key (Position) REFERENCES Positions(ID)
-)
+DELETE FROM Doctors
+where ID = 3 OR ID = 4
 
-create table Parents(
-	ID int not null primary key,
-	parName Varchar(20),
-	phone varchar(10),
-	egn varchar(10)
-)
+select * from Pacients
 
-create table Pacients(
-	ID int not null primary key,
-	pacName varchar(20),
-	phone Varchar(10),
-	egn varchar(10),
-	Parent int,
-	Doctor int,
+select * from Reservations
 
-	Foreign key (Parent) REFERENCES Parents(ID),
-	Foreign key (Doctor) REFERENCES Doctors(ID)
-)
-
-Create table Doctors(
-	ID int not null primary key,
-	workerName varchar(20),
-	phone varchar(10),
-	email varchar(20),
-	salary float,
-)
-
-Create table Reservations(
-	ID int not null,
-	thedate smalldatetime, /*YYYY-MM-DD hh:mm:ss*/
-	Patient int,
-	Doctor int,
-
-	Foreign key (Patient) REFERENCES Pacients(ID),
-	Foreign key (Doctor) REFERENCES Doctors(ID)
-)
+DELETE FROM Reservations
