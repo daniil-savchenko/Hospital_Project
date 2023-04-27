f03ec4647c9fb2b0795d4e697fc4ca503f3d280b
Student <Student@DESKTOP-LS45GV1>
2023-04-27 08:58:25 +0300

    added value to Combobox

--- Hospital_Project/PrintForm.cs
+++ Hospital_Project/PrintForm.cs
@@ -20,6 +20,11 @@ namespace Hospital_Project
     {
         protected string tablename;
         private int id;
+        private static string constring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"" + Path.GetFullPath(Path.Combine(Path.GetFullPath(Directory.GetCurrentDirectory()), @"..\..\Hospital_database.mdf")) + "\";Integrated Security=True;Connect Timeout=30";
+        private static SqlConnection con = new SqlConnection(constring);
+        private SqlDataAdapter adb;
+        private DataTable table;
+
         public PrintForm()
         {
             InitializeComponent();
@@ -183,18 +188,20 @@ namespace Hospital_Project
         private void button1_Click(object sender, EventArgs e)
         {
             
-            MessageBox.Show(dataGridView.SelectedCells[0].Value.ToString()); // value
+            /*MessageBox.Show(dataGridView.SelectedCells[0].Value.ToString()); // value
             MessageBox.Show(dataGridView.Columns[dataGridView.CurrentCell.ColumnIndex].Name); // name of column
-            MessageBox.Show(id.ToString());
+            MessageBox.Show(id.ToString());*/
+
+            MessageBox.Show(comboBoxpacPar.ValueMember);
             
-            string columnname = dataGridView.Columns[dataGridView.CurrentCell.ColumnIndex].Name;
+            /*string columnname = dataGridView.Columns[dataGridView.CurrentCell.ColumnIndex].Name;
             var newval = "";
             DataBaseManager cmd = new DataBaseManager();
             
             if (cmd.UpdateData(tablename, columnname, newval, id.ToString()))
             {
                 MessageBox.Show("Success");
-            }
+            }*/
         }
 
         private void dataGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
@@ -227,6 +234,23 @@ namespace Hospital_Project
             panel5.Hide();
             panel6.Hide();
             panel7.Hide();
+
+            var select = "SELECT * FROM Parents";
+            using (SqlCommand cmd = new SqlCommand(select, con))
+            {
+                adb = new SqlDataAdapter(cmd);
+                table = new DataTable();
+                adb.Fill(table);
+                adb.Dispose();
+            }
+            Dictionary<string, string> test = new Dictionary<string, string>();
+            comboBoxpacPar.DisplayMember = "Text";
+            comboBoxpacPar.ValueMember = "Value";
+            foreach (DataRow row in table.Rows)
+            {
+                comboBoxpacPar.Items.Add(new { Value = row["ID"].ToString(), Text = row["parName".ToString()] });
+            }
+
         }
     }
 }
